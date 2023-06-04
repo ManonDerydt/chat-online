941ab9d5f4dc9d3d4a3d79585427e4e6541e4c3a
manon <derydtmanon@gmail.com>
2023-06-05 00:28:01 +0200

    Premier commit

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ app.get("/", (req, res) => {
 io.on('connection', (socket) => {
     console.log('Un utilisateur s\'est connecté');
 
-    socket.on('chat message', (msg) => {
-        console.log("Message :" + msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', (data) => {
+        console.log("Message de " + data.username + " : " + data.message);
+        io.emit('chat message', data);
     });
-
-
 });
+
 
 server.listen(3000, () => {
     console.log("Le serveur tourne sur le port 3000")
